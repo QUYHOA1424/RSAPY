37bb98916d0e0a857cf1ad1a51a305265b5ce227
Hung Le <lethaihung142@gmail.com>
2024-11-18 00:34:56 +0700

    Clarify, add normalize_residude function

--- basic_math_funcs.py
+++ basic_math_funcs.py
@@ -28,7 +28,9 @@ def gcd(a, b):
         a, b = b, a % b
     return a
 
-# return gcd,x,y where a.x + b.y = gcd
+# Return gcd,x,y where a.x + b.y = gcd
+# Use for finding a^-1 (mod b) -> x
+# Use for finding b^-1 (mod a) -> y
 def extended_gcd(a, b):
     if a == 0:
         return b, 0, 1
@@ -37,17 +39,23 @@ def extended_gcd(a, b):
     y = x1
     return gcd, x, y
 
+# Ensure x represent for the residue class (x > 0)
+def normalize_residue(x, mod): 
+	result = x % mod 
+	return result if result >= 0 else result + mod
+
 # return e^-1 (mod phi)
 def mod_inverse(e, phi):
     gcd, x, y = extended_gcd(e, phi)
     if gcd != 1:
         raise Exception('Modular inverse does not exist')
     else:
-        return x % phi
+	    x = normalize_residue(x, phi)
+	    return x
 
 if __name__ == "__main__":
-	a = 7
-	b = 5
+	a = 38454
+	b = 743
 	c = 8
 	gcd0 = gcd(a,b)
 	gcd1, x, y = extended_gcd(a,b)
